176488fb53c0caf02bf551f2eb86ddf7ebc282b5
Jeanette Henriette BURGER <rburger@e4r20p1.wethinkcode.co.za>
2018-10-15 15:07:02 +0200

    starting Companion with rush00 info

---
+++ companion/Forum/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  tables
+//
+//  Created by Jeanette Henriette BURGER on 2018/10/04.
+//  Copyright © 2018 Jeanette Henriette BURGER. All rights reserved.
+//
+
+import UIKit
+
+var code: String = "code"
+var accessToken: String = "token"
+var token: String = ""
+var userToken: String = ""
+
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    @IBAction func Logout(_ sender: UIBarButtonItem) {
+        code = ""
+        accessToken = ""
+        userToken = ""
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = UIColor(patternImage: #imageLiteral(resourceName: "WTC-4-crop.jpg"))
+        let apiCall = ApiController();
+        apiCall.getToken();
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    var data = Data.info
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return Data.info.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "New Topic")
+        cell?.textLabel?.text = Data.info[indexPath.row].0
+        cell?.detailTextLabel?.text = Data.info[indexPath.row].1
+        return cell!
+    }
+}
+
